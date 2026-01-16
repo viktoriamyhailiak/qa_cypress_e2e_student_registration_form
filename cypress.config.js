28c60ba41ce7aa440e46c87885ceae73d78d1de6
Viktoriia <viktoriamyhailiak@gmail.com>
2026-01-16 20:56:04 +0200

    add a solution

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-    }
+    },
+    baseUrl: 'https://demoqa.com/automation-practice-form'
   }
 });
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,36 @@
+/* eslint-disable max-len */
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
-  before(() => {
-
+  beforeEach(() => {
+    cy.visit('');
   });
 
-  it('', () => {
+  it('should fill the registration form and submit', () => {
+    cy.get('#firstName').type('Viktoriia');
+    cy.get('#lastName').type('Mykhailiak');
+    cy.get('#userEmail').type('viktoriia.mykhailiak@gmail.com');
+    cy.get('label[for="gender-radio-2"]').click();
+    cy.get('#userNumber').type('0635537015');
 
+    cy.get('#dateOfBirthInput').click();
+    cy.get('.react-datepicker__month-select').select('September');
+    cy.get('.react-datepicker__year-select').select('2005');
+    cy.get('.react-datepicker__day--001')
+      .not('.react-datepicker__day--outside-month')
+      .click();
+
+    cy.get('#subjectsInput').type('JavaScript{enter}', { force: true });
+
+    cy.get('label[for="hobbies-checkbox-1"]').click({ force: true });
+
+    cy.get('#currentAddress').type('Lviv, Ukraine', { force: true });
+
+    cy.get('#react-select-3-input').type('NCR{enter}', { force: true });
+    cy.get('#react-select-4-input').type('Delhi{enter}', { force: true });
+
+    cy.get('#submit').click({ force: true });
+    cy.contains('Thanks for submitting the form').scrollIntoView();
+    cy.contains('Thanks for submitting the form').should('be.visible');
   });
 });
